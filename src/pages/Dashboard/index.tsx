0464f5d4167fa89276b60a0898e3e2bce02bd313
Stephany <tete.1818@hotmail.com>
2020-05-29 19:53:30 -0300

    CRUD

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get<IFoodPlate[]>('foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +39,17 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const { image, name, price, description } = food;
+
+      const response = await api.post<IFoodPlate>('foods', {
+        image,
+        name,
+        price,
+        description,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +58,32 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const { image, name, price, description } = food;
+
+    const response = await api.put<IFoodPlate>(`/foods/${editingFood.id}`, {
+      ...editingFood,
+      image,
+      name,
+      price,
+      description,
+    });
+
+    const updatedFood = response.data;
+
+    setFoods(state =>
+      state.map(f => {
+        if (f.id === updatedFood.id) {
+          return updatedFood;
+        }
+        return f;
+      }),
+    );
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    setFoods(state => state.filter(f => f.id !== id));
   }
 
   function toggleModal(): void {
@@ -62,7 +95,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
